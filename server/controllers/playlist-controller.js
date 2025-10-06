a238e0840d31272bbdb54a933492df588a93ac81
Elvin Ly <elvinly323@gmail.com>
2025-10-06 02:38:00 -0400

    Implement queries

--- server/controllers/playlist-controller.js
+++ server/controllers/playlist-controller.js
@@ -61,6 +61,7 @@ readAllPlaylists = async (req, res) => {
         return res.status(200).json({ success: true, data: playlists })
     }).catch(err => console.log(err))
 }
+
 readPlaylistPairs = async (req, res) => {
     await Playlist.find({}, (err, playlists) => {
         if (err) {
@@ -87,9 +88,127 @@ readPlaylistPairs = async (req, res) => {
     }).catch(err => console.log(err))
 }
 
+updatePlaylist = async (req, res) => {
+    const body = req.body;
+    console.log("updatePlaylist body: " + JSON.stringify(body));
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a Playlist',
+        })
+    }
+
+    await Playlist.findOneAndUpdate({ _id: req.params.id }, body, { new: true }, (err, playlist) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!playlist) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        return res.status(200).json({
+            success: true,
+            playlist: playlist,
+            message: 'Playlist Updated!',
+        })
+    }
+    ).catch(err => console.log(err))
+}
+
+deletePlaylist = async (req, res) => {
+    await Playlist.findOneAndDelete({ _id: req.params.id }, (err, playlist) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!playlist) {
+            return res.status(404).json({
+                success: false,
+                error: 'Playlist not found'
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            playlist: playlist,
+            message: 'Playlist Deleted!',
+        })
+    }).catch(err => console.log(err))
+}
+
+queryPlaylistsByPrefix = async (req, res) => {
+    const prefix = req.query.prefix;
+
+    if (!prefix) {
+        return res.status(400).json({
+            success: false,
+            error: 'Prefix query parameter is required',
+        })
+    }
+
+    await Playlist.find({ name: { $regex: `^${prefix}`, $options: 'i' } }, (err, playlists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!playlists.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Playlists not found` })
+        }
+        return res.status(200).json({ success: true, data: playlists })
+    }
+    ).catch(err => console.log(err))
+}
+
+querySongs = async (req, res) => {
+    const { title, artist, year } = req.query;
+
+    await Playlist.find({}, (err, playlists) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+        if (!playlists.length) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Playlists not found` })
+        }
+
+        let res = [];
+        let seen = new Set();
+
+        playlists.forEach(playlist => {
+            playlist.songs.forEach(song => {
+                let metCriteria = false;
+
+                if ((title && song.title.toLowerCase().includes(title.toLowerCase())) ||
+                    (artist && song.artist.toLowerCase().includes(artist.toLowerCase())) ||
+                    (year && song.year == parseInt(year))) {
+                    metCriteria = true;
+                }
+
+                if (metCriteria === true) {
+                    let key = `${song.title}${song.artist}${song.year}`;
+                    if (!seen.has(key)) { //if we alr have this song, then its a dupe and we dont add it to the result
+                        seen.add(key);
+                        res.push(song);
+                    }
+                }
+            });
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: allSongs,
+            count: allSongs.length
+        })
+    }).catch(err => console.log(err))
+}
+
 module.exports = {
     createPlaylist,
     readAllPlaylists,
+    queryPlaylistsByPrefix,
+    querySongs,
     readPlaylistPairs,
     readPlaylistById,
+    updatePlaylist,
+    deletePlaylist,
 }
--- server/routes/playlists-router.js
+++ server/routes/playlists-router.js
@@ -11,7 +11,11 @@ const router = express.Router()
 // FOR USE WITH PLAYLISTER NOW
 router.post('/playlist', PlaylistController.createPlaylist)
 router.get('/playlist/:id', PlaylistController.readPlaylistById)
+router.put('/playlist/:id', PlaylistController.updatePlaylist)
+router.delete('/playlist/:id', PlaylistController.deletePlaylist)
 router.get('/playlists', PlaylistController.readAllPlaylists)
+router.get('/playlists/search', PlaylistController.queryPlaylistsByPrefix)
+router.get('/playlists/songs', PlaylistController.querySongs)
 router.get('/playlistpairs', PlaylistController.readPlaylistPairs)
 
 module.exports = router
